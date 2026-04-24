f898f591f46178cdb0e4fb06905e6bbf1d05d332
Test-techP <pauline.puga.pp@gmail.com>
2026-04-24 16:42:27 +0200

    Ajout modale, filtres dynamiques et gestion UI login

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Js/Homepage.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Js/Homepage.js
@@ -1,7 +1,6 @@
 const URL =  "http://localhost:5678/api/";
-
 const gallery = document.querySelector(".gallery");
-
+const modalGallery = document.querySelector(".modal-gallery");
 
 function FetchProject () {
     fetch (URL+"works") 
@@ -11,7 +10,9 @@ function FetchProject () {
         for (let i = 0; i<works.length;i++){
             CreateProjects(works[i]);
         }
+        createFilters(works);
     } )
+    
 }
 
 function CreateProjects (work){
@@ -26,8 +27,127 @@ function CreateProjects (work){
     figure.appendChild(figcaption);
 }
 
-//utiliser fetchprojets pour afficher la gallerie d'images dans la popup 
+function getCategories(works) {
+    const categories = [];
 
-//CreateProjects ();
+    works.forEach(work => {
+        if (!categories.includes(work.category.name)) {
+            categories.push(work.category.name);
+        }
+    });
+
+    return categories;
+}
+
+function addFilterEvents() {
+    const buttons = document.querySelectorAll(".filters button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            filterWorks(button.dataset.filter);
+        });
+    });
+}
+
+function filterWorks(category) {
+    gallery.innerHTML = "";
+
+    let filteredWorks = workList;
+
+    if (category !== "all") {
+        filteredWorks = workList.filter(work =>
+            work.category.name === category
+        );
+    }
+
+    filteredWorks.forEach(work => {
+        CreateProjects(work);
+    });
+}
+
+
+function createFilters(works) {
+    const filtersContainer = document.querySelector(".filters");
+
+    filtersContainer.innerHTML = "";
+
+    // bouton "Tous"
+    const allBtn = document.createElement("button");
+    allBtn.textContent = "Tous";
+    allBtn.dataset.filter = "all";
+    filtersContainer.appendChild(allBtn);
+
+    const categories = getCategories(works);
+
+    categories.forEach(cat => {
+        const btn = document.createElement("button");
+        btn.textContent = cat;
+        btn.dataset.filter = cat;
+        filtersContainer.appendChild(btn);
+    });
+
+    addFilterEvents();
+}
 
 FetchProject ();
+
+//page loggée ou non
+
+let isLoggedIn = true;
+
+function updateUI() {
+    const filters = document.querySelector(".filters");
+    const editMode = document.querySelector(".edit-mode");
+    const editButton = document.querySelector(".edit-button");
+
+    if (isLoggedIn) {
+        filters.style.display = "none";
+        editMode.classList.remove("hidden");
+        editButton.classList.remove("hidden");
+    } else {
+        filters.style.display = "block";
+        editMode.classList.add("hidden");
+        editButton.classList.add("hidden");
+    }
+}
+
+updateUI();
+
+//modales
+
+const modal = document.querySelector(".modal");
+const btnEdit = document.querySelector(".edit-button");
+const btnClose = document.querySelector(".close");
+
+btnEdit.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+btnClose.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+const view1 = document.querySelector(".content-gallery");
+const view2 = document.querySelector(".content-add-photo");
+const btnAdd = document.querySelector(".btn-open-add");
+
+btnAdd.addEventListener("click", () => {
+  view1.classList.add("hidden");
+  view2.classList.remove("hidden");
+});
+
+function CreateModalItem(work) {
+    const div = document.createElement("div");
+
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+
+    const title = document.createElement("p");
+    title.textContent = work.title;
+
+    div.appendChild(img);
+    div.appendChild(title);
+
+    modalGallery.appendChild(div);
+}
